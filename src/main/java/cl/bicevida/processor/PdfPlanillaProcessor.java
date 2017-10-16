0f5b2522306ec6bd17266f6ea4290b8930b97af8
bv-gustavo-riquelme <gustavo.riquelmeg@bicevida.cl>
2017-10-16 11:18:14 -0300

    Cambio en la configuracion del boton prima

--- src/main/java/cl/bicevida/processor/PdfPlanillaProcessor.java
+++ src/main/java/cl/bicevida/processor/PdfPlanillaProcessor.java
@@ -2,15 +2,19 @@ package cl.bicevida.processor;
 
 
 
+import java.io.IOException;
+
 import javax.ws.rs.WebApplicationException;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import cl.bicevida.view.planillacaja.ws.GetUrlPDFResponse;
 import cl.bicevida.util.CreateCxfClient;
+import cl.bicevida.util.GetProperties;
 import cl.bicevida.view.planillacaja.ws.PlanillaCajaWS;
 
 public class PdfPlanillaProcessor implements Processor{
@@ -32,13 +36,21 @@ public class PdfPlanillaProcessor implements Processor{
 				
 				GetUrlPDFResponse outD = new GetUrlPDFResponse();
 
-				PlanillaCajaWS cliente = CreateCxfClient.createCXFClient(url);
+				PlanillaCajaWS cliente = createCXFClient(url);
 				outD.setReturn((cliente.getUrlPDF(rut)));
 				ex.getOut().setBody(outD);
 			}
 		} catch (Exception e) {
 			throw new WebApplicationException(e);
 		}
+	}
+	public static PlanillaCajaWS createCXFClient(String url) throws IOException{
+		
+		
+		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+		factory.setServiceClass(PlanillaCajaWS.class);
+		factory.setAddress(url);
+		return (PlanillaCajaWS)factory.create();		
 	}
 
 }
